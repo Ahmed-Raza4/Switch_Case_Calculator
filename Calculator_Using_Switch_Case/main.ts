a593ac07f838b0100e6b9c750afd40f3df7fe4c0
Ahmed-Raza4 <hafizahmedraza12345@gmail.com>
2024-03-21 01:25:14 +0500

    Switch Case 1.0.1

---
+++ Calculator_Using_Switch_Case/main.ts
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+//step 01
+const answer : {
+    numberOne: number,
+    numberTwo: number,
+    operator: string,  
+} = await inquirer.prompt([
+    {type: "number",
+    name: "numberOne",
+    message: chalk.green.bold("Kindly Enter your First Number")
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: chalk.green.bold("Kindly Enter Your Second Number")
+    },
+    {
+        type: "list",
+        name: "operator",
+        message: chalk.red.bold("Enter your operator"),
+        choices: ["+", "-", "*", "/"]
+    }
+])
+
+//step 02 Switch Case
+
+const {numberOne, numberTwo, operator} = answer
+
+let result 
+switch (operator) {
+    case "+":
+        result = numberOne + numberTwo;
+        break;
+    case "-":
+        result = numberOne - numberTwo;
+        break;
+    case "*":
+        result = numberOne*numberTwo;
+        break;
+    case "/":
+        result = numberOne / numberTwo;
+        break;
+    default:
+        console.log(chalk.red.bold("Invalid operator"));
+}
+
+console.log(chalk.cyan.bold(`${numberOne} ${operator} ${numberTwo} = ${result}`));
